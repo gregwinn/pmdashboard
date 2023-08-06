0ac5018db2d5ad992c208be5c164decf6aa06782
Greg Winn <winn.greg@gmail.com>
2023-08-06 15:27:19 -0500

    chore: adjusting cancan based on reqs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,20 +15,11 @@ class Ability
     end
 
     if user.employee?
-      can :create, WorkTask
-      can :update, WorkTask do |work_task|
-        work_task.user_id == user.id
-      end
-      can :destroy, WorkTask do |work_task|
-        work_task.user_id == user.id
-      end
-
       can :update, WorkTask do |work_task|
         if work_task.status_changed?
-
           (work_task.status == 'working' || work_task.status == 'needs_review') && work_task.user_assignment_id == user.id
         else
-          work_task.user_id == user.id
+          work_task.user_id == user.id && work_task.status != 'done'
         end
       end
     end
