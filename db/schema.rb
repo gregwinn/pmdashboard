c776d1ac091ebde22f8cc49a370dd47a52ef378c
Greg Winn <winn.greg@gmail.com>
2023-08-05 20:38:57 -0500

    chore: schema and enabled devise scoped views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_185126) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_225048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_185126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -51,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_185126) do
     t.datetime "updated_at", null: false
     t.bigint "project_id", null: false
     t.date "due_date"
+    t.integer "user_assignment_id"
     t.index ["project_id"], name: "index_work_tasks_on_project_id"
     t.index ["user_id"], name: "index_work_tasks_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 # Test users for development and demo purposes
 # You can run rails db:seed to create these users
-pm_user = {email: "pm_test@test.com", password: "test1234", role: :pm}
-employee_user = {email: "employee_test@test.com", password: "test1234", role: :employee}
+pm_user = {email: "pm_test@test.com", password: "test1234", role: :pm, first_name: "Michael", last_name: "Scott"}
+employee_user = {email: "employee_test@test.com", password: "test1234", role: :employee, first_name: "Dwight", last_name: "Schrute"}
 User.create([pm_user, employee_user])
